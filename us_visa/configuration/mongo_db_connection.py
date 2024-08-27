ed005f1848e7bda044c6b62ed5734061f599bff0
Fredy Rojas <frojas.r60@gmail.com>
2024-08-28 01:14:07 +1200

    solving issue with getting key values from system environment, pending define if read the key value directly in constant

--- us_visa/configuration/mongo_db_connection.py
+++ us_visa/configuration/mongo_db_connection.py
@@ -24,13 +24,20 @@ class MongoDBClient:
     """
     client = None
 
-    def __init__(self, database_name=DATABASE_NAME) -> None:
+    def __init__(self, database_name=None) -> None:
+
+        # Pending to improve how to initialize environment Key secrets values. 
+        database_name = os.environ.get(DATABASE_NAME)
+        
         try:
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)
                 if mongo_db_url is None:
                     raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
-                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url, 
+                                                        #    tlsCAFile=ca
+                                                           )
+                
             self.client = MongoDBClient.client
             self.database = self.client[database_name]
             self.database_name = database_name
